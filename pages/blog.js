f410f29e79cc391d5be40a0839dc311e116153bb
curgery <androidgeek54@gmail.com>
2020-10-28 20:35:11 -0400

    commit completed to displaying data

---
+++ pages/blog.js
@@ -0,0 +1,24 @@
+import Layout from '../components/Layout';
+import Link from 'next/link';
+
+
+const PostLink = ({title}) => (
+    <li>
+        <Link href={`/post?title=${title}`}>
+            <a>{title}</a>
+        </Link>
+    </li>
+);
+
+
+const Blog = () => (
+    <Layout title="My Blog">
+        <ul>
+            <PostLink title="react"/>
+            <PostLink title="angular"/>
+            <PostLink title="vue"/>
+        </ul>
+    </Layout>
+)
+
+export default Blog
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+import Layout from '../components/Layout'
+
+
 const Index = () => (
-    <div>Welcome to NextJS</div>
+    <Layout title="Home">
+    <Link href="/about">
+        <a>Go to about</a>
+    </Link>
+        <p>Welcome to NextJS Home Page</p>
+    </Layout>
 )
 
 
